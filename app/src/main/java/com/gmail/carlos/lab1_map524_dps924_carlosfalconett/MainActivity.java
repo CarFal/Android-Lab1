596b2ba46868f9fa318c350f5ec2c13c98d6da52
CarFal <57931255+CarFal@users.noreply.github.com>
2021-03-02 17:18:17 -0500

    First Commit.

---
+++ app/src/main/java/com/gmail/carlos/lab1_map524_dps924_carlosfalconett/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.carlos.lab1_map524_dps924_carlosfalconett;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/gmail/carlos/lab1_map524_dps924_carlosfalconett/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.gmail.carlos.lab1_map524_dps924_carlosfalconett;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
